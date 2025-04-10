37c1cb61149a69c11e28ea8d6c27e3d1ad31d4ba
AyushChopra24 <ayushchopra_24@yahoo.com>
2025-04-10 09:52:54 -0400

    Revalidating the path in admin folder

--- app/admin/delete/[id]/route.js
+++ app/admin/delete/[id]/route.js
@@ -1,3 +1,5 @@
+// app/admin/delete/[id]/route.js
+
 import { revalidatePath } from "next/cache";
 
 export async function POST(_, { params }) {
@@ -6,6 +8,12 @@ export async function POST(_, { params }) {
   const res = await fetch(`http://127.0.0.1:4000/footy/${id}`, {
     method: "DELETE",
   });
+
+  if (res.ok) {
+    revalidatePath("/admin");
+    revalidatePath("/collection");
+    revalidatePath(`/collection/${id}`);
+  }
 
   return new Response(null, {
     status: 302,
